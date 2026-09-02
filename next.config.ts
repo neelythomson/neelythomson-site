2441af05260aa4395eaf9a7f499f9f81714b628d
neelythomson <neelyannconway@gmail.com>
2026-09-01 23:37:01 -0400

    CSP: fonts from self only

--- next.config.ts
+++ next.config.ts
@@ -32,8 +32,8 @@ const securityHeaders = [
     value: [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline'",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "font-src 'self' https://fonts.gstatic.com",
+      "style-src 'self' 'unsafe-inline'",
+      "font-src 'self'",
       "img-src 'self' data: https:",
       "connect-src 'self'",
       "frame-src https://neelythomson.substack.com",
